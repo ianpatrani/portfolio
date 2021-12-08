2df53a06e22268011ebb79e3694c1c667dc42332
ianpatrani <ianpatrani@hotmail.com>
2021-12-08 12:27:05 -0300

    Ready

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -7,7 +7,7 @@ import '../accordion.css'
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "./Themes.js";
 import { useTranslation } from 'react-i18next';
-
+import '../index.css';
 const StyledApp = styled.div`
   color: ${(props) => props.theme.fontColor};
 `;
@@ -24,20 +24,23 @@ const MyPage = () => {
     const [t] = useTranslation("global");//t es el texto traducido
 
     return (
-        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-            <GlobalStyles />
-            <StyledApp >
-                <button onClick={() => themeToggler()}>💡Turn Off</button>
-                <div className="App">
-                    <Header />
-                    <Accordion title={t("title1.text1")} p={t("content1.description1")} active={active} setActive={setActive} />
-                    <Accordion title={t("title2.text2")} p={t("content2.description2")} active={active} setActive={setActive} />
-                    <Accordion title={t("title3.text3")} p={t("content3.description3")} active={active} setActive={setActive} />
-                    <Accordion title={t("title4.text4")} p={t("content4.description4")} active={active} setActive={setActive} />
-                    <Footer />
-                </div>
-            </StyledApp>
-        </ThemeProvider>
+        <div className="media">
+
+            <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+                <GlobalStyles />
+                <StyledApp >
+                    <button onClick={() => themeToggler()}>💡Turn Off</button>
+                    <div className="App">
+                        <Header />
+                        <Accordion title={t("title1.text1")} p={t("content1.description1")} active={active} setActive={setActive} />
+                        <Accordion title={t("title2.text2")} p={t("content2.description2")} active={active} setActive={setActive} />
+                        <Accordion title={t("title3.text3")} p={t("content3.description3")} active={active} setActive={setActive} />
+                        <Accordion title={t("title4.text4")} p={t("content4.description4")} active={active} setActive={setActive} />
+                        <Footer />
+                    </div>
+                </StyledApp>
+            </ThemeProvider>
+        </div>
     );
 }
 
